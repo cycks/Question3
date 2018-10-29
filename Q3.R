5d7c6f000d543e135213601bf407f7e64b696860
Sikolia Atswenje Wycliffe <sikolia.wycliffe@gmail.com>
2018-10-29 16:05:39 +0300

    Edit Q3.R file to find count based on ratios

--- Q3.R
+++ Q3.R
@@ -1,7 +1,8 @@
+setwd("E:/projects/data incubator/QuestionThree/Question3")
 libs = c("readr", "dplyr", "tidyverse", "magrittr", "tidyr", "ggplot2", "rgeos",
          "usmap")
 lapply(libs, require, character.only = TRUE)
-sentences<- read_csv("E:/projects/data incubator/QuestionThree/sentences.csv")
+alldata<- read_csv("sentences.csv")
 
 ###########################################
 ### Clean the Data
@@ -13,12 +14,12 @@ alldata<-subset(alldata, select = c("defendant", "year", "state", "county", "def
 alldata[which(alldata[,3]=='Floridaorida', arr.ind=TRUE), 3] <- 'Florida'
 alldata[which(alldata[,3]=='Nebraskabraska', arr.ind=TRUE), 3] <- 'Nebraska'
 
-
+View(alldata)
 ########################################################
 # Create heat Map
 
-alldata.map<- alldata %>% group_by(state) %>% mutate(count = n())
-use.for.map <- unique(subset(alldata.map, select=c(state, count)))
+alldata.map<- alldata %>% group_by(state, year) %>% mutate(count = n())
+use.for.map <- unique(subset(alldata.map, select=c(state, count, year)))
 View(use.for.map)
 
 plot_usmap(data = use.for.map, values = "count", regions = "states") + 
@@ -27,9 +28,12 @@ plot_usmap(data = use.for.map, values = "count", regions = "states") +
   scale_fill_continuous(name = "Number of Death Sentences", label = scales::comma) + 
   theme(legend.position = "right")
 
-use.for.map1<- subset(use.for.map, count>=100)
+use.for.map1<- subset(use.for.map, count>=30)
 View(use.for.map)
-
+calculateraio<-function(count, population){
+  ratio<-(count/population)*1000000
+  return(ratio)
+}
 
 ggplot(data=use.for.map1, aes(x=sort(state), y=count)) +
   geom_bar(fill="#DD8888", width=.8, stat="identity")+
@@ -49,11 +53,23 @@ ggplot(data=use.for.graph, aes(x=year, y=count)) +
   ggtitle("A Bar Graph Showng the Number of Death Sentences by Year")
 
 
+population_2000 <- read_csv("population_2000.csv")
+
+population_2010 <- read_csv("population_2010.csv")
+population_2017 <- read_csv("population_2017.csv")
+
+population_2010 <- aggregate(. ~ state, data=population_2010, FUN=sum)
 
 
+population_2000$state==population_2010$state
+population_2017$state==population_2010$state
 
+populationdata<-merge(population_2000, population_2010, by = 'state')
+populationdata<-merge(populationdata, population_2017, by = 'state')
 
+colSums(populationdata[sapply(populationdata, is.numeric)], na.rm = TRUE)
 
+View(populationdata)
 
 
 
